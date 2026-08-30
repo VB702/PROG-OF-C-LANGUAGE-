a5c0c6dde7ba2c02319a20fbfb925a72abfbf4da
VB702 <vinaybhatt.may@gmail.com>
2026-08-30 18:51:53 +0530

    Added ASCII Value and Factorial programs

---
+++ ASCII VALUE.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+
+int main()  {
+    
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    printf("ASCII value of %c = %d\n", ch, ch);
+
+    printf("Program Made By: Vinay Bhatt\n");
+
+
+    return 0;
+}
