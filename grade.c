56502e040aa922a711a3d687a080cf1e94a19527
VB702 <vinaybhatt.may@gmail.com>
2026-09-06 21:07:24 +0530

    LINK

---
+++ grade.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int marks;
+
+    printf("Enter marks: ");
+    scanf("%d", &marks);
+
+    if(marks >= 80)
+        printf("Excellent, Grade A\n");
+    else if(marks >= 60)
+        printf("Grade B\n");
+    else if(marks >= 40)
+        printf("Grade C\n");
+    else
+        printf("Fail\n");
+
+    printf("Made by - Vinay Bhatt");
+
+    return 0;
+}
