ac065246f1cded25c0dd6cf741ea4c8cb28c90b9
VB702 <vinaybhatt.may@gmail.com>
2026-09-06 21:07:58 +0530

    LINK

---
+++ quadratic equation.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c, d, r1, r2;
+
+    printf("Enter a, b and c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    d = b * b - 4 * a * c;
+
+    if(d > 0)
+    {
+        r1 = (-b + sqrt(d)) / (2 * a);
+        r2 = (-b - sqrt(d)) / (2 * a);
+
+        printf("Root 1 = %.2f\n", r1);
+        printf("Root 2 = %.2f\n", r2);
+    }
+    else if(d == 0)
+    {
+        r1 = -b / (2 * a);
+
+        printf("Root 1 = %.2f\n", r1);
+        printf("Root 2 = %.2f\n", r1);
+    }
+    else
+    {
+        printf("Roots are imaginary\n");
+    }
+
+    printf("Made by - Vinay Bhatt");
+
+    return 0;
+}
+
+    
