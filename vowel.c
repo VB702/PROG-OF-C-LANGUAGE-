bdcd756cb10531b665bb49e9fc1eeda3d1d3675e
VB702 <vinaybhatt.may@gmail.com>
2026-09-14 11:23:54 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    switch(ch)
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            printf("It is a vowel\n");
+            break;
+
+        default:
+            printf("It is not a vowel\n");
+    }
+
+    printf("Program Made by - Vinay Bhatt");
+
+    return 0;
+}
