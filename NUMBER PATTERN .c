309270a1fe269079815452bbc50358ea1d2b5801
VB702 <vinaybhatt.may@gmail.com>
2026-09-21 10:10:11 +0530

    link >

---
+++ NUMBER PATTERN .c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+
+        for(j = i - 1; j >= 1; j--)
+        {
+            printf("%d ", j);
+        }
+
+        printf("\n");
+    }
+
+    printf("Program Made By - Vinay Bhatt");
+
+    return 0;
+}
